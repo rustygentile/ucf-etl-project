b8e23053b3992fefdaecc0b7803f598213d87af4
Minal J <minaljajoo0108@gmail.com>
2018-12-12 17:57:42 -0500

    website with all region teams info

--- website/app.py
+++ website/app.py
@@ -1,54 +1,44 @@
 from flask import Flask, render_template, redirect  
 import os
-import pymongo
 import json
-from flask_pymongo import PyMongo
-#import scrape_costa
+
 
 # Create an instance of Flask
 app = Flask(__name__, static_url_path='/static')
 
+# Read all the data for all 4 regions
+east_team = json.load(open('static/Resources/east_teams.json'))
+west_team = json.load(open('static/Resources/west_teams.json'))
+midwest_team = json.load(open('static/Resources/midwest_teams.json'))
+south_team = json.load(open('static/Resources/south_teams.json'))
 
-east_team = json.load(open('east_team_logos.json'))
-# setup mongo connection
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
-
-# connect to mongo db and collection
-db = client.team_mascot_db
-collection_south = db.teams_south
-collection_east = db.teams_east
-collection_west = db.teams_west
-collection_midwest = db.teams_midwest
-
-#east_team= list(db.teams_east.find())
-
-
-# Route to render index.html template using data from Mongo
+######################
 @app.route("/")
-
 def home():
-    
-    # Return template and data
-    return render_template("index.html")
 
+    return render_template("index.html")
+#--------------------
 @app.route("/east")
-def region():
+def east_region():
     
     return render_template("east.html",east_team=east_team)
-# Route that will trigger the scrape function
-#@app.route("/scrape")
-#def scrape():
+#--------------------
+@app.route("/west")
+def west_region():
+    
+    return render_template("west.html",west_team=west_team)
+#--------------------
+@app.route("/south")
+def south_region():
+    
+    return render_template("south.html",south_team=south_team)
+#--------------------
+@app.route("/midwest")
+def midwest_region():
+    
+    return render_template("midwest.html",midwest_team=midwest_team)
 
-    # Run the scrape function
-    #ncaa_data = scrape_ncaa.scrape_info()
-
-    # Update the Mongo database using update and upsert=True
-   # mongo.db.collection.update({}, ncaa_data, upsert=True)
-
-    # Redirect back to home page
-   # return redirect("/")
-
+######################
 
 if __name__ == "__main__":
     app.run(debug=True)
--- website/scrape_teams.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-# Setup connection to mongodb
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
-
-# Define database and collection
-db = client.team_mascot_db
-collection = db.teams
